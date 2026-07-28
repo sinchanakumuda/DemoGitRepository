755f2771fd7b50b94491552b98e325d97a9b2b1c
sinch <sinch@Sinchana_21>
2026-07-28 12:42:35 +0530

    DeveloperJuni2 adding

---
+++ Pre_Programming/src/basic_programs/DeveloperJuni2.java
@@ -0,0 +1,12 @@
+package basic_programs;
+
+public class DeveloperJuni2 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Everyone I am Junior 2");
+	}
+
+
+}
